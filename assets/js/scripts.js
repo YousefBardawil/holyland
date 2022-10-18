0090db0a19822cc2b082a6c7d243a1cecba73b9e
author <yousefalbardawil6@gmail.com>
2022-10-18 22:55:57 +0300

    four

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -1,45 +1,5 @@
-
-function scroll_to(clicked_link, nav_height) {
-	var element_class = clicked_link.attr('href').replace('#', '.');
-	var scroll_to = 0;
-	if(element_class != '.top-content') {
-		element_class += '-container';
-		scroll_to = $(element_class).offset().top - nav_height;
-	}
-	if($(window).scrollTop() != scroll_to) {
-		$('html, body').stop().animate({scrollTop: scroll_to}, 1000);
-	}
-}
-
-
-jQuery(document).ready(function() {
 	
-	/*
-	    Navigation
-	*/
-	$('a.scroll-link').on('click', function(e) {
-		e.preventDefault();
-		scroll_to($(this), $('nav').outerHeight());
-	});
 	
-    /*
-        Background
-    */
-    $('.section-4-container').backstretch("assets/img/backgrounds/bg.jpg");
-    
-    /*
-	    Wow
-	*/
-	new WOW().init();
-	
-	/*
-
-	
-	    Carousel
-	*/
-
-
-
 	$('#carousel-example').on('slide.bs.carousel', function (e) {
 
 	    /*
@@ -68,7 +28,9 @@ jQuery(document).ready(function() {
 
 
 
+
 	$('#carousel-example1').on('slide.bs.carousel', function (e) {
+
 
 	    /*
 	        CC 2.0 License Iatek LLC 2018
@@ -232,7 +194,3 @@ jQuery(document).ready(function() {
 
 	
 	
-});
-
-
-
